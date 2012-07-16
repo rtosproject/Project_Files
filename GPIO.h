5acc268f4cb525e0d4173ef095c380e82debbe7d
Allen <allengu12@gmail.com>
2012-07-16 11:38:04 -0400

    added GPIO header file (GPIO.h)

---
+++ GPIO.h
@@ -0,0 +1,45 @@
+//GPIO assignments
+
+//=====================Expansion Header P8 Pinout=====================
+#define GPIO1_6 	38	//CONN 3
+#define GPIO1_7 	39	//CONN 4
+#define GPIO1_2		34	//CONN 5
+#define GPIO1_3		35	//CONN 6
+#define GPIO1_13	45	//CONN 11
+#define GPIO1_12	44	//CONN 12
+#define GPIO0_26	26	//CONN 14
+#define GPIO1_15	47	//CONN 15
+#define GPIO1_14	46	//CONN 16
+#define GPIO0_27	27	//CONN 17
+#define GPIO2_1		65	//CONN 18
+#define GPIO1_31	63	//CONN 20
+#define GPIO1_30	62	//CONN 21
+#define GPIO1_5		37	//CONN 22
+#define GPIO1_4		36	//CONN 23
+#define GPIO1_1		33	//CONN 24
+#define GPIO1_0		32	//CONN 25
+#define GPIO1_29	61	//CONN 26
+#define GPIO2_22	86	//CONN 27
+#define GPIO2_24	88	//CONN 28
+#define GPIO2_23	87	//CONN 29
+#define GPIO2_25	89	//CONN 30
+#define GPIO2_12	76	//CONN 39
+#define GPIO2_13	77	//CONN 40
+#define GPIO2_10	74	//CONN 41
+#define GPIO2_11	75	//CONN 42
+#define GPIO2_8		72	//CONN 43
+#define GPIO2_9		73	//CONN 44
+#define GPIO2_6		70	//CONN 45
+#define GPIO2_7		71	//CONN 46
+
+
+
+//=====================Expansion Header P9 Pinout=====================
+#define GPIO1_28	60	//CONN 12
+#define GPIO1_16	48	//CONN 15
+#define GPIO1_17	49	//CONN 23
+#define GPIO3_21	117	//CONN 25
+#define GPIO3_19	115	//CONN 27
+#define GPIO0_7		7	//CONN 42
+
+
